aed8475c4bc1c795c796f93ceb4dd3f418021f8a
Adam Piotrowski <adam@wellcommerce.org>
2017-02-21 01:12:29 +0100

    Changed .gitsync

--- gitsync.sh
+++ gitsync.sh
@@ -43,39 +43,3 @@ if [ "$action" == 'push' ]
             git subtree push --prefix=src/WellCommerce/Bundle/$i git@github.com:WellCommerce/$i.git master
         done
 fi
-
-if [ "$action" == 'pull' ]
-    then
-        git commit -a -m "Pushing changes before pulling subtrees"
-
-        for i in "${components[@]}"
-        do
-            git subtree pull --prefix=src/WellCommerce/Component/$i git@github.com:WellCommerce/$i.git master
-        done
-
-        for i in "${bundles[@]}"
-        do
-            git subtree pull --prefix=src/WellCommerce/Bundle/$i git@github.com:WellCommerce/$i.git master
-        done
-fi
-
-if [ "$action" == 'release' ]
-    then
-        git commit -a -m "Pushing changes before synchronizing subtrees"
-
-        for i in "${components[@]}"
-        do
-            git subtree push --prefix=src/WellCommerce/Component/$i git@github.com:WellCommerce/$i.git master
-        done
-
-        for i in "${bundles[@]}"
-        do
-            git subtree push --prefix=src/WellCommerce/Bundle/$i git@github.com:WellCommerce/$i.git master
-        done
-
-        sleep 3m
-
-        php -d memory_limit=4096M composer.phar update
-        git commit -a -m "Updated composer.lock file"
-        git push
-fi
