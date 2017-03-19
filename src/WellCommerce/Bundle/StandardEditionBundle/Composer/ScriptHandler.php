877342649d83bb06c27f3d1b644ca7058a421845
Adam Piotrowski <adam@wellcommerce.org>
2017-03-19 12:57:29 +0100

    Added auto-generation of extra traits during app install/update

--- src/WellCommerce/Bundle/StandardEditionBundle/Composer/ScriptHandler.php
+++ src/WellCommerce/Bundle/StandardEditionBundle/Composer/ScriptHandler.php
@@ -13,8 +13,12 @@
 namespace WellCommerce\Bundle\StandardEditionBundle\Composer;
 
 use Composer\Package\Package;
+use Composer\Repository\InstalledRepositoryInterface;
 use Composer\Script\Event;
+use gossi\codegen\generator\CodeGenerator;
+use gossi\codegen\model\PhpTrait;
 use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler as SensioScriptHandler;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Class ScriptHandler
@@ -28,13 +32,17 @@ class ScriptHandler extends SensioScriptHandler
         $options    = static::getOptions($event);
         $consoleDir = static::getConsoleDir($event, 'Install WellCommerce');
         
+        static::generateEnhancerTraits($event);
+        
         static::executeCommand($event, $consoleDir, 'wellcommerce:install', $options['process-timeout']);
     }
     
     public static function updateApplication(Event $event)
     {
-        $options       = static::getOptions($event);
-        $consoleDir    = static::getConsoleDir($event, 'Update WellCommerce');
+        $options    = static::getOptions($event);
+        $consoleDir = static::getConsoleDir($event, 'Update WellCommerce');
+        
+        static::generateEnhancerTraits($event);
         
         static::executeCommand($event, $consoleDir, 'wellcommerce:doctrine:enhance', $options['process-timeout']);
     }
@@ -50,5 +58,41 @@ class ScriptHandler extends SensioScriptHandler
         $io = $event->getIO();
         $io->write('SYMFONY_SECRET=' . getenv('SYMFONY_SECRET'));
         $io->write('SEARCH_INDEX_PREFIX=' . getenv('SEARCH_INDEX_PREFIX'));
+    }
+    
+    protected static function generateEnhancerTraits(Event $event)
+    {
+        /** @var InstalledRepositoryInterface $installedRepo */
+        $installedRepo = $event->getComposer()->getRepositoryManager()->getLocalRepository();
+        $io            = $event->getIO();
+        $fs            = new Filesystem();
+        
+        /** @var Package $package */
+        foreach ($installedRepo->getCanonicalPackages() as $package) {
+            $enhancerTraits = $package->getExtra()['enhancer-traits'] ?? [];
+            if (count($enhancerTraits)) {
+                foreach ($enhancerTraits as $traitClass => $targetFilename) {
+                    if (false === $fs->exists($targetFilename)) {
+                        $io->write(sprintf('<info>Generating %s for %s</info>', $traitClass, $package->getName()));
+                        $fs->dumpFile($targetFilename, static::generateTrait($traitClass));
+                    }
+                }
+            }
+        }
+    }
+    
+    protected static function generateTrait(string $traitClass): string
+    {
+        $trait = new PhpTrait($traitClass);
+        
+        $generator = new CodeGenerator([
+            'generateDocblock'        => false,
+            'generateEmptyDocblock'   => false,
+            'generateScalarTypeHints' => true,
+            'generateReturnTypeHints' => true,
+            'propertySorting'         => true,
+        ]);
+        
+        return '<?php' . str_repeat(PHP_EOL, 2) . $generator->generate($trait);
     }
 }
