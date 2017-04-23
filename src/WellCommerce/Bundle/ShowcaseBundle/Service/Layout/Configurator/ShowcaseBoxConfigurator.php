83ef72f76b7975a258bc7b274828542cafde809f
Adam Piotrowski <adam@wellcommerce.org>
2017-04-23 12:35:55 +0200

    Moved configurator to Service\Layout namespace

--- src/WellCommerce/Bundle/ShowcaseBundle/Service/Layout/Configurator/ShowcaseBoxConfigurator.php
+++ src/WellCommerce/Bundle/ShowcaseBundle/Service/Layout/Configurator/ShowcaseBoxConfigurator.php
@@ -10,7 +10,7 @@
  * please view the LICENSE file that was distributed with this source code.
  */
 
-namespace WellCommerce\Bundle\ShowcaseBundle\Configurator;
+namespace WellCommerce\Bundle\ShowcaseBundle\Service\Layout\Configurator;
 
 use WellCommerce\Bundle\CatalogBundle\DataSet\Admin\CategoryDataSet;
 use WellCommerce\Bundle\CatalogBundle\DataSet\Admin\ProductStatusDataSet;
