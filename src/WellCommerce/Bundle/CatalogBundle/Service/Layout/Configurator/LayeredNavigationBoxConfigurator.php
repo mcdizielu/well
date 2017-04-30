e62e05cac8f8f2eed59a411111e173bfae690a7f
Adam Piotrowski <adam@wellcommerce.org>
2017-05-01 00:50:39 +0200

    Added status filter to layered navigation box

--- src/WellCommerce/Bundle/CatalogBundle/Service/Layout/Configurator/LayeredNavigationBoxConfigurator.php
+++ src/WellCommerce/Bundle/CatalogBundle/Service/Layout/Configurator/LayeredNavigationBoxConfigurator.php
@@ -47,16 +47,19 @@ final class LayeredNavigationBoxConfigurator extends AbstractLayoutBoxConfigurat
             'label'   => 'layout_box.layered_navigation.enable_price_filter',
         ]))->setValue($this->getValue($defaults, '[enable_price_filter]', true));
     
-    
         $fieldset->addChild($builder->getElement('checkbox', [
             'name'    => 'enable_brand_filter',
             'label'   => 'layout_box.layered_navigation.enable_brand_filter',
         ]))->setValue($this->getValue($defaults, '[enable_brand_filter]', true));
     
-    
         $fieldset->addChild($builder->getElement('checkbox', [
             'name'    => 'enable_attribute_filter',
             'label'   => 'layout_box.layered_navigation.enable_attribute_filter',
         ]))->setValue($this->getValue($defaults, '[enable_attribute_filter]', true));
+
+        $fieldset->addChild($builder->getElement('checkbox', [
+            'name'    => 'enable_status_filter',
+            'label'   => 'layout_box.layered_navigation.enable_status_filter',
+        ]))->setValue($this->getValue($defaults, '[enable_status_filter]', true));
     }
 }
