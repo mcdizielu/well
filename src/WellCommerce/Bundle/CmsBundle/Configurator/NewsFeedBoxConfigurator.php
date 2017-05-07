0f0965fd223dd60c373261952752dafcbadb19b2
Adam Piotrowski <adam@wellcommerce.org>
2017-05-07 11:09:09 +0200

    NewsFeedBoxConfigurator CS

--- src/WellCommerce/Bundle/CmsBundle/Configurator/NewsFeedBoxConfigurator.php
+++ src/WellCommerce/Bundle/CmsBundle/Configurator/NewsFeedBoxConfigurator.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: diversantvlz
- * Date: 19.04.2017
- * Time: 18:37
- */
 
 namespace WellCommerce\Bundle\CmsBundle\Configurator;
 
@@ -13,23 +7,28 @@ use WellCommerce\Bundle\CoreBundle\Layout\Configurator\AbstractLayoutBoxConfigur
 use WellCommerce\Component\Form\Elements\FormInterface;
 use WellCommerce\Component\Form\FormBuilderInterface;
 
+/**
+ * Class NewsFeedBoxConfigurator
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
 class NewsFeedBoxConfigurator extends AbstractLayoutBoxConfigurator
 {
-
+    
     public function __construct(NewsFeedBoxController $controller)
     {
         $this->controller = $controller;
     }
-
+    
     public function getType(): string
     {
         return 'NewsFeed';
     }
-
+    
     public function addFormFields(FormBuilderInterface $builder, FormInterface $form, $defaults)
     {
         $fieldset = $this->getFieldset($builder, $form);
-
+        
         $fieldset->addChild($builder->getElement('text_field', [
             'name'  => 'per_page',
             'label' => 'news_feed.layout_box.per_page',
